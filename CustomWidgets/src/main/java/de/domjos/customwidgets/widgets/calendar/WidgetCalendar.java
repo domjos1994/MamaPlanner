de71bcfebdf39b4350526763bb9280864a5b49f1
domjos1994 <developing@domjos.de>
2019-11-04 20:55:27 +0100

    update app

--- CustomWidgets/src/main/java/de/domjos/customwidgets/widgets/calendar/WidgetCalendar.java
+++ CustomWidgets/src/main/java/de/domjos/customwidgets/widgets/calendar/WidgetCalendar.java
@@ -6,10 +6,7 @@ import android.os.Build;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.ViewGroup;
-import android.widget.HorizontalScrollView;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.*;
 
 import androidx.gridlayout.widget.GridLayout;
 
@@ -23,6 +20,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
+import com.google.android.material.navigation.NavigationView;
 import de.domjos.customwidgets.R;
 import de.domjos.customwidgets.utils.MessageHelper;
 import de.domjos.customwidgets.utils.WidgetUtils;
@@ -31,9 +29,10 @@ public class WidgetCalendar extends LinearLayout {
     private Context context;
     private ImageButton cmdCalSkipNext, cmdCalNext, cmdCalPrevious, cmdCalSkipPrevious;
     private TextView lblCalDate;
-    private GridLayout gridLayout;
     private SimpleDateFormat dateFormat;
     private SimpleDateFormat dateFormatWithDay;
+    private TableLayout tableLayout;
+    private List<TableRow> tblRows;
     private List<Map.Entry<String, Event>> events;
     private ClickListener clickListener;
     private Event currentEvent;
@@ -213,17 +212,20 @@ public class WidgetCalendar extends LinearLayout {
         textView.setBackgroundColor(WidgetUtils.getColor(this.context, android.R.color.background_dark));
         this.addView(textView);
 
-        HorizontalScrollView horizontalScrollView = new HorizontalScrollView(this.context);
-        horizontalScrollView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        this.addView(horizontalScrollView);
+        this.tableLayout = new TableLayout(this.context);
+        this.tableLayout.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        this.tableLayout.setWeightSum(7);
+        this.addView(this.tableLayout);
 
-        this.gridLayout = new GridLayout(this.context);
-        LinearLayout.LayoutParams layout = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        this.gridLayout.setLayoutParams(layout);
-        this.gridLayout.setColumnCount(7);
-        this.gridLayout.setOrientation(GridLayout.HORIZONTAL);
-        this.gridLayout.setAlignmentMode(GridLayout.ALIGN_BOUNDS);
-        horizontalScrollView.addView(this.gridLayout);
+        this.tblRows = new LinkedList<>();
+        for(int i = 0; i<=5; i++) {
+            TableRow tblRow = new TableRow(this.context);
+            tblRow.setBackgroundColor(WidgetUtils.getColor(this.context, android.R.color.background_dark));
+            tblRow.addView(this.addTextView("testz"));
+            this.tableLayout.addView(tblRow);
+            this.tblRows.add(tblRow);
+        }
+
         this.reloadCalendar();
     }
 
@@ -231,24 +233,28 @@ public class WidgetCalendar extends LinearLayout {
         DateFormatSymbols dateFormatSymbols = new DateFormatSymbols(Locale.getDefault());
         for(String dayOfWeek : dateFormatSymbols.getWeekdays()) {
             if(dayOfWeek.length()>=3) {
-                this.gridLayout.addView(this.addTextView(dayOfWeek.substring(0, 3)));
+                ((TableRow) this.tableLayout.getChildAt(0)).addView(this.addTextView(dayOfWeek.substring(0, 3)));
             }
         }
     }
 
     private void reloadCalendar() {
         try {
-            this.gridLayout.removeAllViews();
+            for(TableRow tbl : this.tblRows) {
+                tbl.removeAllViews();
+            }
             this.addDaysOfWeek();
 
             Calendar calendar = this.getDefaultCalendar();
             int day = calendar.get(Calendar.DAY_OF_WEEK);
             for(int i = day-1; i>0; i--) {
-                this.gridLayout.addView(this.addTextView(""));
+                this.tblRows.get(1).addView(this.addTextView(""));
             }
             for(int i = 1; i<=calendar.getActualMaximum(Calendar.DAY_OF_MONTH); i++) {
-                this.gridLayout.addView(this.addDay(i));
+                int row = i / 7;
+                this.tblRows.get(row).addView(this.addDay(i));
             }
+            this.tableLayout.invalidate();
         } catch (Exception ex) {
             MessageHelper.printException(ex, this.context);
         }
@@ -257,15 +263,7 @@ public class WidgetCalendar extends LinearLayout {
     private TextView addTextView(String text) {
         TextView lbl = new TextView(this.context);
         lbl.setText(text);
-        GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams();
-        layoutParams.setGravity(Gravity.CENTER_HORIZONTAL);
-        layoutParams.setMargins(5, 5, 5, 5);
-        layoutParams.columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1);
-        lbl.setLayoutParams(layoutParams);
-        lbl.setGravity(Gravity.CENTER_HORIZONTAL);
-        lbl.setTextSize(20);
-        lbl.setPadding(5, 5, 5, 5);
-        lbl.setTypeface(null, Typeface.BOLD);
+        lbl.setLayoutParams(new LayoutParams(0, LayoutParams.WRAP_CONTENT, 1));
         return lbl;
     }
 
@@ -284,20 +282,24 @@ public class WidgetCalendar extends LinearLayout {
         LinearLayout linearLayout = new LinearLayout(this.context);
         linearLayout.setOrientation(VERTICAL);
         linearLayout.setGravity(Gravity.CENTER);
-
-        GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams();
-        layoutParams.setGravity(Gravity.CENTER_HORIZONTAL);
-        layoutParams.columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1);
-        layoutParams.setMargins(5, 5, 5, 5);
-        linearLayout.setLayoutParams(layoutParams);
+        linearLayout.setLayoutParams(new LinearLayout.LayoutParams(0, LayoutParams.WRAP_CONTENT, 1));
         linearLayout.setPadding(5, 5, 5, 5);
         linearLayout.addView(this.addTextViewToDay(String.valueOf(currentDayOfMonth), android.R.color.transparent));
+
         linearLayout.setOnClickListener(view -> {
-            for(int i = 0; i<=this.gridLayout.getChildCount()-1; i++) {
+            int items = 0;
+            for(TableRow tbl : this.tblRows) {
+                items += tbl.getChildCount();
+            }
+
+            for(int i = 0; i<=items-1; i++) {
+                int row = (i - 7) / 7;
+                int column = (i - 7) % 7;
+
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                    this.gridLayout.getChildAt(i).setBackground(WidgetUtils.getDrawable(this.context, -1));
+                    this.tblRows.get(row).getChildAt(column).setBackground(WidgetUtils.getDrawable(this.context, -1));
                 } else {
-                    this.gridLayout.getChildAt(i).setBackgroundDrawable(WidgetUtils.getDrawable(this.context, -1));
+                    this.tblRows.get(row).getChildAt(column).setBackgroundDrawable(WidgetUtils.getDrawable(this.context, -1));
                 }
             }
             linearLayout.setBackgroundColor(WidgetUtils.getColor(this.context, android.R.color.darker_gray));
